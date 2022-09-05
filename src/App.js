0dff7d450d9d415cf4b581345ba22e2154cabe0d
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2022-09-05 12:27:34 +0200

    dice game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const DiceGame = () => {
+  const [players, setPlayers] = useState([
+    { id: 1, name: "Player1", score: 0, currentThrow: null },
+    { id: 2, name: "Player2", score: 0, currentThrow: null },
+  ]);
+
+  const [winner, setWinner] = useState("");
+
+  const rollDice = (id) => {
+    const rolling = players.map((player) => {
+      return id === player.id
+        ? {
+            ...player,
+            currentThrow: Math.floor(Math.random() * 6) + 1,
+          }
+        : player;
+    });
+    setPlayers(rolling);
+  };
+
+  const compareScore = () => {
+    const highestScore = players.reduce((a, b) => {
+      return a.currentThrow > b.currentThrow
+        ? a
+        : a.currentThrow < b.currentThrow
+        ? b
+        : "It's a tie";
+    });
+    setWinner(highestScore.name ? highestScore.name : "It's a tie");
+
+    const points = players.map((player) => {
+      return player.id === highestScore.id
+        ? { ...player, score: player.score + 1, currentThrow: 0 }
+        : { ...player, currentThrow: 0 };
+    });
+    setPlayers(points);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Dice Game</h1>
+      {players.map((player) => {
+        return (
+          <div>
+            <p>
+              {player.name}, current:{" "}
+              {player.currentThrow ? player.currentThrow : "Roll the dice"}{" "}
+              <button onClick={() => rollDice(player.id)}>Roll dice</button>{" "}
+              <b>Points: {player.score}</b>
+            </p>
+          </div>
+        );
+      })}
+      <button onClick={compareScore}>Compare Score</button>
+      {winner && <h2>{winner}</h2>}
     </div>
   );
-}
+};
 
-export default App;
+export default DiceGame;
